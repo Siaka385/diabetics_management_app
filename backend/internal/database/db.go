89d5a6a7750e3b124b5245104202b2dc380fa984
jamos <amosjoel91@gmail.com>
2024-12-14 00:49:28 +0300

    feat: add values to a database

--- backend/internal/database/db.go
+++ backend/internal/database/db.go
@@ -1,1 +1,25 @@
 package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+
+func initDB(dataSourceName string) error {
+	var err error
+	DB, err = sql.Open("sqlite3", dataSourceName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
