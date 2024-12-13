02c15bc8e71b4e38facac33e975cefbf93a6c1b7
jamos <amosjoel91@gmail.com>
2024-12-13 23:47:48 +0300

    refactor: restructure NewMedicationService() for use in database

--- backend/internal/services/medication.go
+++ backend/internal/services/medication.go
@@ -1,1 +1,31 @@
 package services
+
+import (
+	"time"
+)
+
+type Users struct {
+	user_id  string `json:"user_id"`
+	name     string `json:"name"`
+	email    string `json:"email"`
+	password string `json:"password"`
+}
+
+type Medication struct {
+	medication_id    string    `json:"medication_id"`
+	user_id          string    `json:"user_id"`
+	medication_name  string    `json:"medication_name"`
+	dose             string    `json:"dose"`
+	dosage_time      time.Time `json:"time"`
+	dosage_frequency string    `json:"frequency"`
+	notes            string    `json:"notes"`
+}
+
+type MedicationService struct{}
+
+// new medication creates a new medication
+func NewMedicationService() *MedicationService {
+	return &MedicationService{}
+}
+
+// Adding medication to the database
