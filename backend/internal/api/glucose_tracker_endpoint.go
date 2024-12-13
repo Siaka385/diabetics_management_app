9ddda1f6a34d914446193dc5f105e4ac72954334
davodhiambo <jesseomolo@yahoo.com>
2024-12-13 21:29:34 +0300

    feat:add GlucoseTrackerEndPointHandler.

--- backend/internal/api/glucose_tracker_endpoint.go
+++ backend/internal/api/glucose_tracker_endpoint.go
@@ -1,1 +1,19 @@
 package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func GlucoseTrackerEndPointHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Capture glucose level and date from the request query parameters
+	glucoseLevel := r.URL.Query().Get("glucose")
+	glucoseDate := r.URL.Query().Get("date")
+
+	glucoseParam := map[string]string{glucoseLevel: glucoseDate}
+
+	// Set response header and JSON encode the glucose level and date
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(glucoseParam)
+}
