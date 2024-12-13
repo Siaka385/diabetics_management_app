b77f531c83d1467a0327858a5be75c7e3731726a
johnotieno0 <poptiff@gmail.com>
2024-12-14 00:36:44 +0300

    test: for edit plan endpoint handler

--- backend/internal/api/handlers_test.go
+++ backend/internal/api/handlers_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -85,5 +86,58 @@ func TestGetDefaultMealPlan(t *testing.T) {
 	}
 	if !reflect.DeepEqual(gotResponse, expectedResponse) {
 		t.Errorf("Expected '%v' got '%v'", expectedResponse, gotResponse)
+	}
+}
+
+func TestEditPlan(t *testing.T) {
+	edit := MealPlan{
+		Name:        "Diabetic-Friendly Breakfast",
+		Calories:    300,
+		Protein:     25.0,
+		Carbs:       20.0,
+		Fats:        10.0,
+		Ingredients: []string{"Milk", "Whole Wheat Bread", "Molly"},
+	}
+	data, err := json.Marshal(edit)
+	if err != nil {
+		t.Fatalf("Failed to serialize input data: %v", err)
+	}
+	req, err := http.NewRequest("POST", "/nutrition/editplan", bytes.NewReader(data))
+	if err != nil {
+		t.Fatalf("Failed to create request: %v", err)
+	}
+	handler := http.HandlerFunc(EditPlan)
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+	if err := rr.Code; err != http.StatusOK {
+		t.Error("Handler returned wrong status")
+	}
+
+	expectedResp := map[string]string{"message": "Meal plan updated successfully"}
+	var gotResp map[string]string
+	err = json.Unmarshal(rr.Body.Bytes(), &gotResp)
+	if err != nil {
+		t.Fatalf("Failed to deserialize response: %v", err)
+	}
+	if !reflect.DeepEqual(gotResp, expectedResp) {
+		t.Errorf("Expected '%v' got '%v'", expectedResp, gotResp)
+	}
+}
+
+func TestLogMealHandler(t *testing.T) {
+	type args struct {
+		w http.ResponseWriter
+		r *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			LogMealHandler(tt.args.w, tt.args.r)
+		})
 	}
 }
