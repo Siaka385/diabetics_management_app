3c640847eb22f72177a9d13b8961025029d8120a
johnotieno0 <poptiff@gmail.com>
2024-12-14 16:15:48 +0300

    fix: remove unwanted lines

--- backend/internal/api/handlers.go
+++ backend/internal/api/handlers.go
@@ -295,10 +295,6 @@ func GenerateMealInsights(nutrientInfo NutrientInfo) string {
  */
 func Index(db *gorm.DB, tmpl *template.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// auth.RegisterUser(db, "toni", "toni@mail.com", "antony102")
-		// auth.RegisterUser(db, "toni", "toni@mail.com", "antony102")
-		// auth.LoginUser(db, "toni", "antony102")
-
 		var templateName string
 		templateName = "index.html"
 
