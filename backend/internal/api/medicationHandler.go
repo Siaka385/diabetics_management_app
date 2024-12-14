f9d14dfc9ed720108327c9bd428e3027ec733d51
jamos <amosjoel91@gmail.com>
2024-12-14 14:41:47 +0300

    feat: add handler for deleting medication

--- backend/internal/api/medicationHandler.go
+++ backend/internal/api/medicationHandler.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"diawise/internal/services"
@@ -19,7 +18,6 @@ func AddMedication(db *gorm.DB) http.HandlerFunc {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		fmt.Println("MED: ", med)
 
 		newMed, err := services.AddMedication(db, med)
 		if err != nil {
@@ -28,6 +26,25 @@ func AddMedication(db *gorm.DB) http.HandlerFunc {
 		}
 		w.WriteHeader(http.StatusCreated)
 		json.NewEncoder(w).Encode(newMed)
+	}
+}
+
+// handler for deleting a medication
+func DeleteMedication(db *gorm.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var med services.Medication
+
+		if err := json.NewDecoder(r.Body).Decode(&med); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		err := services.DeleteMedication(db, med)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
 	}
 }
 
