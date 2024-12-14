441c45f55aa0de9dfb8bd860e64b161ba5e07645
tesiaka <tsiaka7197@stu.kemu.ac.ke>
2024-12-14 20:57:14 +0300

    ft:add routers

--- backend/internal/api/handlers.go
+++ backend/internal/api/handlers.go
@@ -1,8 +1,8 @@
 package api
 
 import (
-	"net/http"
 	"html/template"
+	"net/http"
 
 	// auth "diawise/internal/auth"
 
@@ -20,6 +20,81 @@ func Index(db *gorm.DB, tmpl *template.Template) http.HandlerFunc {
 
 		var templateName string
 		templateName = "index.html"
+
+		err := tmpl.ExecuteTemplate(w, templateName, nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+}
+
+func BloodSugar(db *gorm.DB, tmpl *template.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// auth.RegisterUser(db, "toni", "toni@mail.com", "antony102")
+		// auth.LoginUser(db, "toni", "antony102")
+
+		var templateName string
+		templateName = "bloodsugar.html"
+
+		err := tmpl.ExecuteTemplate(w, templateName, nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+}
+
+func DietAndNutrient(db *gorm.DB, tmpl *template.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// auth.RegisterUser(db, "toni", "toni@mail.com", "antony102")
+		// auth.LoginUser(db, "toni", "antony102")
+
+		var templateName string
+		templateName = "NutritionAndDiet.html"
+
+		err := tmpl.ExecuteTemplate(w, templateName, nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+}
+
+func Medicine(db *gorm.DB, tmpl *template.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// auth.RegisterUser(db, "toni", "toni@mail.com", "antony102")
+		// auth.LoginUser(db, "toni", "antony102")
+
+		var templateName string
+		templateName = "Medication.html"
+
+		err := tmpl.ExecuteTemplate(w, templateName, nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+}
+
+func Community(db *gorm.DB, tmpl *template.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// auth.RegisterUser(db, "toni", "toni@mail.com", "antony102")
+		// auth.LoginUser(db, "toni", "antony102")
+
+		var templateName string
+		templateName = "CommunityAndSupport.html"
+
+		err := tmpl.ExecuteTemplate(w, templateName, nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+}
+
+func Education(db *gorm.DB, tmpl *template.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// auth.RegisterUser(db, "toni", "toni@mail.com", "antony102")
+		// auth.LoginUser(db, "toni", "antony102")
+
+		var templateName string
+		templateName = "Education.html"
 
 		err := tmpl.ExecuteTemplate(w, templateName, nil)
 		if err != nil {
