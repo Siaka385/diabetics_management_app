c033d8b80e1c9c219e4543e142a503efcb188a6d
jamos <amosjoel91@gmail.com>
2024-12-14 13:00:26 +0300

    fix: fix error in db connection to function

--- backend/internal/api/medicationHandler.go
+++ backend/internal/api/medicationHandler.go
@@ -6,6 +6,8 @@ import (
 	"time"
 
 	"diawise/internal/services"
+
+	"gorm.io/gorm"
 )
 
 type MedicationHandler struct {
@@ -18,27 +20,29 @@ func NewMedicalHandler(services *services.MedicationService) *MedicationHandler 
 }
 
 // handler for adding a new medication
-func (h *MedicationHandler) AddMedication(w http.ResponseWriter, r *http.Request) {
-	var med struct {
-		Medication_id    string    `json:"medication_id"`
-		User_id          string    `json:"user_id"`
-		Medication_name  string    `json:"medication_name"`
-		Dose             string    `json:"dose"`
-		Dosage_time      time.Time `json:"time"`
-		Dosage_frequency string    `json:"frequency"`
-		Notes            string    `json:"notes"`
-	}
+func (h *MedicationHandler) AddMedication(db *gorm.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var med struct {
+			Medication_id    string    `json:"medication_id"`
+			User_id          string    `json:"user_id"`
+			Medication_name  string    `json:"medication_name"`
+			Dose             string    `json:"dose"`
+			Dosage_time      time.Time `json:"time"`
+			Dosage_frequency string    `json:"frequency"`
+			Notes            string    `json:"notes"`
+		}
 
-	if err := json.NewDecoder(r.Body).Decode(&med); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
+		if err := json.NewDecoder(r.Body).Decode(&med); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 
-	newMed, err := h.service.AddMedication(services.Medication{})
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		newMed, err := h.service.AddMedication(services.Medication{})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(newMed)
 	}
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(newMed)
 }
